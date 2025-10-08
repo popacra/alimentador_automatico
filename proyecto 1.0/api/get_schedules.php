26883536df5ea3a039a2668d5d230be607581452
Sebastian Soliz <sebashionkaito@gmail.com>
2025-10-09 05:53:31 +0800

    Delete proyecto 1.0 directory

--- proyecto 1.0/api/get_schedules.php
+++
@@ -1,9 +0,0 @@
-<?php
-    require_once('config.php')
-    $stmt = $pdo-> prepare('SELECT * FROM feeder_schedule ORDER BY updated_at DESC');
-    $stmt->execute();
-    $result = $stmt->fetch(); 
-
-    
-?>
-
